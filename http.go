18e959a153508e490f495b20a1d60174fcd919cc
dongguijun <dongguijun1994>
2020-12-27 10:32:18 +0800

    go http请求修改

--- http.go
+++ http.go
@@ -19,7 +19,7 @@ func Curl(url, method string, data interface{}, headerMap map[string]string, act
 		req.Header.Add(k, v)
 	}
 	if err != nil {
-		panic(err)
+		return nil, err
 	}
 	defer req.Body.Close()
 
@@ -31,7 +31,7 @@ func Curl(url, method string, data interface{}, headerMap map[string]string, act
 	resp, err := client.Do(req)
 	defer resp.Body.Close()
 	if err != nil {
-		panic(err)
+		return nil, err
 	}
 	result, _ := ioutil.ReadAll(resp.Body)
 	content := string(result)
@@ -40,5 +40,41 @@ func Curl(url, method string, data interface{}, headerMap map[string]string, act
 		return act, nil
 	} else {
 		return nil, err
+	}
+}
+
+func CurlRes(url, method string, data interface{}, headerMap map[string]string, act interface{}) (interface{}, *http.Response, error) {
+
+	//序列化数据 对象或者map
+	jsonStr, _ := json.Marshal(data)
+	//创建请求兑对象
+	req, err := http.NewRequest(method, url, bytes.NewBuffer(jsonStr))
+	//设置请求头
+	for k, v := range headerMap {
+		req.Header.Add(k, v)
+	}
+	if err != nil {
+		return nil, nil, err
+	}
+	defer req.Body.Close()
+
+	//封装客户端对象 设置超时为15秒
+	client := http.Client{
+		Timeout: 15 * time.Second,
+	}
+	//请求
+	resp, err := client.Do(req)
+
+	defer resp.Body.Close()
+	if err != nil {
+		return nil, nil, err
+	}
+	result, _ := ioutil.ReadAll(resp.Body)
+	content := string(result)
+	//封装数据
+	if err := json.Unmarshal([]byte(content), &act); err == nil {
+		return act, resp, nil
+	} else {
+		return nil, nil, err
 	}
 }
